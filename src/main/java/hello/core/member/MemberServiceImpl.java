8cdde81f77f272647be1f111e08ba3059031a795
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-06 23:32:37 +0900

    DIP 생성
    서비스내에서 추상클래스를 구현한객체 참조를 하여 가공 및 사용하는게 아니라 ( 의존하여 사용X )
    
    인터페이스 필드와 필드의 생성자를 만들고
    하나의 클래스에서 인터페이스를 관리를하며 구현한 생성자를 주입해준다. ( 의존관계 주입 )
    
    이렇게 되면 서비스단에서는 인터페이스가 어느 객체를 참조하든 말든 자기 기능만 하면되는것이다.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,15 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*기존 객체참조를 하지않고( 의존하지 않고 ) 필드를 생성자로 만들고 AppConfig에서 관리 -> 추상화에만 의존 DIP, 생성자 주입*/
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // AppConfig보면 memberRepository 매개변수에 new MemoryMemberRepository()가 들어가있으므로 this.memberRepository에는 Memory가 들어가고 위 필드에 메모리 생성자가 주입됨.
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,29 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    public final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /*고정할인 DiscountPolicy가 FixDiscountPolicy를 참조(의존) 에서
+    * 퍼센트할인로변경하려면 ReateDiscountPolicy를 참조(의존)하게 코드를 바꿔야해서 OCP위반*/
+//    public final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    public final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    /*기존 객체참조를 하지않고(의존하지않고) 필드를 생성자로 만들고 AppConfig에서 관리 -> 추상화에만 의존 DIP , 생성자 주입*/
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    /*APP Config보면 memberRepository에는 MemoryMemberRe~~ , discountPolicy에는 Fixdisco~~ 가 생성되있으므로
+    * 아래 생성자 매게변수에 메모리 ,픽스가 들어가 this.memberRepository와 this.discountPolicy에는 각 메모리멤버 , 픽스가 들어가 필드에 생성자가 주입됨.*/
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
