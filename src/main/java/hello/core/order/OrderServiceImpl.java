01482f3d1433dd2339af9bb204bc6111fd7c89cf
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-09 17:46:22 +0900

    Lombok 설치 및 테스트
    게터 세터 생성자 자동생성가능
    생성자같은경우 final붙어야함 그래서 생성자가 하나면 @Autowired도 자동 주입이라 롬복 사용하면 깔끔하게 가능

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component // 구현체에다가 적용해야 스프링 빈에 등록됨.
+@RequiredArgsConstructor // final 붙은 필드들을 생성자로 알아서 만들어줌
 public class OrderServiceImpl implements OrderService {
     /*고정할인 DiscountPolicy가 FixDiscountPolicy를 참조(의존) 에서
     * 퍼센트할인로변경하려면 ReateDiscountPolicy를 참조(의존)하게 코드를 바꿔야해서 OCP위반*/
@@ -23,11 +25,11 @@ public class OrderServiceImpl implements OrderService {
 
     /*APP Config보면 memberRepository에는 MemoryMemberRe~~ , discountPolicy에는 Fixdisco~~ 가 생성되있으므로
     * 아래 생성자 매게변수에 메모리 ,픽스가 들어가 this.memberRepository와 this.discountPolicy에는 각 메모리멤버 , 픽스가 들어가 필드에 생성자가 주입됨.*/
-    @Autowired // 스프링 빈에 등록된것을 찾아서 타입이 같은걸 주입해줌. , 생성자 하나면 Autowired 안써도 알아서 주입됨.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired // 스프링 빈에 등록된것을 찾아서 타입이 같은걸 주입해줌. , 생성자 하나면 Autowired 안써도 알아서 주입됨.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
