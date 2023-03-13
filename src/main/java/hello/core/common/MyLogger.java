e1d4a605fb1a55e33d88d6a7a878477307b9d914
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-13 14:22:07 +0900

    request 스코프 -1
    http 요청이 오면 로그를 남기기위한 예제
    request 스코프는 클라이언트가 http 요청을 보낼때 생성되는데
    실행결과 오류가뜬다. 아직 클라이언트의 요청이 없으니까

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") // http 요청당 하나씩 생성되며 http 요청 끝나는 시점에 소멸
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("["+uuid +"]"+" ["+requestURL+"] "+message);
+    }
+
+    @PostConstruct // 스코프 지정시 초기화메서드를 사용하자.
+    public void init() {
+        String uuid = UUID.randomUUID().toString();
+        System.out.println("["+uuid +"] request scope been create: "+this);
+    }
+
+    @PreDestroy // 스코프 지정시 종료메서드를 사용하자.
+    public void close() {
+        System.out.println("["+uuid +"] request scope been close: "+this);
+    }
+}
